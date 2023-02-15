9304cfd1a29488295e4cbb3ffe57e61f4c1b2988
Alejandro Drabenche <alejandro.drabenche@header0.com>
2023-02-15 20:21:00 -0300

    Several changes

---
+++ argo-tunnel.sh
@@ -0,0 +1,2 @@
+echo "La clave de admin es: $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+kubectl port-forward svc/argocd-server -n argocd 8080:443
